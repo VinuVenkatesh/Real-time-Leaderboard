40b27a43fced3076d41a8eefbb09f9dd537c3642
VinuVenkatesh <kvsvinuvenkatesh@gmail.com>
2025-12-27 17:11:21 -0500

    Fixed bug where login and register was getting sent to auth middleware

--- backend/middleware/jwt.go
+++ backend/middleware/jwt.go
@@ -9,6 +9,11 @@ import (
 func JWTAuth(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		jwtToken := r.Header.Get("Authorization")
+		url := r.URL.Path
+		if url == "/login" || url == "/register" {
+			next.ServeHTTP(w, r)
+			return
+		}
 		if jwtToken == "" {
 			http.Error(w, "Missing Authorization Header", http.StatusUnauthorized)
 			return
